d3f25dd78558f261a0c20c9a97d20b21310d82fb
Deni Alfarouqi <deni22ti@mahasiswa.pcr.ac.id>
2026-02-18 14:50:56 +0700

    Add: Pre-built frontend assets untuk cPanel

---
+++ public/build/assets/input-error-BXgi0kDB.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Bf1U1RML.js";import{c as o}from"./button-rJHaMioZ.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
