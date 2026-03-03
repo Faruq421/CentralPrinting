2d236b4b7775a0f13930fb5736d89d7a947df2a7
Deni Alfarouqi <deni22ti@mahasiswa.pcr.ac.id>
2026-03-03 13:35:49 +0700

    fix: obfuscate review route to avoid 403 firewall, regenerate ziggy

--- routes/features/review.php
+++ routes/features/review.php
@@ -5,6 +5,6 @@ use App\Features\Review\ReviewController;
 
 Route::resource('reviews', ReviewController::class)->middleware(['auth', 'verified']);
 Route::post('reviews/{review}/update', [ReviewController::class, 'update'])->name('reviews.update.post')->middleware(['auth', 'verified']);
-Route::get('/orders/{order}/review', [ReviewController::class, 'createForOrder'])
+Route::get('/ulasan-produk/{order}', [ReviewController::class, 'createForOrder'])
     ->name('reviews.create-for-order')
     ->middleware(['auth', 'verified']);
